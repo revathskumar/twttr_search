5d5626ff19b51022ef45e74c7d0925b8796ad3c4
Revath S Kumar <rsk@revathskumar.com>
2012-12-12 00:52:31 +0530

    parse tweet and replace the urls with a tag

--- app/scripts/views/tweet_view.js
+++ app/scripts/views/tweet_view.js
@@ -4,8 +4,14 @@ Twitter.Views.Tweet = Backbone.View.extend({
     initialize: function () {
 
     },
+    parseTweet: function (tweet) {
+        var regex = /\b((?:[a-z][\w-]+:(?:\/{1,3}|[a-z0-9%])|www\d{0,3}[.]|[a-z0-9.\-]+[.][a-z]{2,4}\/)(?:[^\s()<>]+|\(([^\s()<>]+|(\([^\s()<>]+\)))*\))+(?:\(([^\s()<>]+|(\([^\s()<>]+\)))*\)|[^\s`!()\[\]{};:'".,<>?«»“”‘’]))/ig;
+        return tweet.replace(regex, function (uri) {
+            return "<a href='" + uri + "'>" + uri + "</a>";
+        });
+    },
     render: function () {
-        $(this.el).append(this.model.get('tweet'));
+        $(this.el).append(this.parseTweet(this.model.get('tweet')));
         return this;
     }
 });
