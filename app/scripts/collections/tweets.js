ab15982a95e828abdc2d95369c651d7fa9318272
Revath S Kumar <rsk@revathskumar.com>
2012-12-15 02:45:37 +0530

    changed the parseTweet to event and added evented url expander

--- app/scripts/collections/tweets.js
+++ app/scripts/collections/tweets.js
@@ -4,6 +4,30 @@ Twitter.Collections.Tweets = Backbone.Collection.extend({
     initialize: function () {
         this.last_tweet = "";
         this.count = 200;
+        this.on('add', this.parseTweet);
+    },
+    expandUrl: function (shortUrl, tweet) {
+        var replaced_text = "";
+
+        $.getJSON("http://jsonpify.heroku.com/?callback=?&resource=" + encodeURIComponent("http://expandurl.appspot.com/expand?url=" + shortUrl), function (data) {
+            var regex = new RegExp(shortUrl, 'g');
+            replaced_text = tweet.get('tweet').replace(regex, decodeURIComponent($.parseJSON(data)['end_url']));
+            tweet.set({tweet: replaced_text }).save();
+        });
+    },
+    parseTweet: function (tweet) {
+
+        var shortUrl = "",
+            regex = /\b((?:[a-z][\w-]+:(?:\/{1,3}|[a-z0-9%])|www\d{0,3}[.]|[a-z0-9.\-]+[.][a-z]{2,4}\/)(?:[^\s()<>]+|\(([^\s()<>]+|(\([^\s()<>]+\)))*\))+(?:\(([^\s()<>]+|(\([^\s()<>]+\)))*\)|[^\s`!()\[\]{};:'".,<>?«»“”‘’]))/ig,
+        updatedTweet = tweet.get('tweet').replace(regex, function (uri) {
+            shortUrl = uri;
+            return "<a href='" + uri + "'>" + uri + "</a>";
+        });
+
+        tweet.set({tweet: updatedTweet }).save();
+        if (shortUrl !== "") {
+            this.expandUrl(shortUrl, tweet);
+        }
     },
     fetch: function (username, options) {
         var max_id = "";
--- app/scripts/views/tweet_view.js
+++ app/scripts/views/tweet_view.js
@@ -2,16 +2,10 @@
 Twitter.Views.Tweet = Backbone.View.extend({
     tagName: "li",
     initialize: function () {
-
-    },
-    parseTweet: function (tweet) {
-        var regex = /\b((?:[a-z][\w-]+:(?:\/{1,3}|[a-z0-9%])|www\d{0,3}[.]|[a-z0-9.\-]+[.][a-z]{2,4}\/)(?:[^\s()<>]+|\(([^\s()<>]+|(\([^\s()<>]+\)))*\))+(?:\(([^\s()<>]+|(\([^\s()<>]+\)))*\)|[^\s`!()\[\]{};:'".,<>?«»“”‘’]))/ig;
-        return tweet.replace(regex, function (uri) {
-            return "<a href='" + uri + "'>" + uri + "</a>";
-        });
+        this.model.on('change', this.render, this);
     },
     render: function () {
-        $(this.el).append(this.parseTweet(this.model.get('tweet')));
+        $(this.el).html(this.model.get('tweet'));
         return this;
     }
 });
